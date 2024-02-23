ccb061fbe9a214f7a4b5b37286adcc3870905005
sinstranger <sinstranger@yandex.ru>
2024-02-23 13:26:33 +0300

    Исправление архитектуры
    Исправил не корректную архитектуру приложения для штатного вывода списка задач

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,8 @@
 import '@assets/sass/App.scss';
 import InputField from '@core/components/InputField';
 import TodoList from '@core/components/TodoList';
-import { useSelector } from 'react-redux';
 
 function App() {
-  const { status, error } = useSelector((state) => state.todos);
-  if (status === 'loading') {
-    return <h1>Loading...</h1>;
-  }
-  if (error) {
-    return <h1>{error}</h1>;
-  }
   return (
     <>
       <InputField />
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -4,11 +4,18 @@ import { fetchTodos } from '@store';
 import { useEffect } from 'react';
 
 function TodoList() {
+  const { status, error } = useSelector((state) => state.todos);
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(fetchTodos());
   }, [dispatch]);
   const todos = useSelector((state) => state.todos.items);
+  if (status === 'loading') {
+    return <h1>Loading...</h1>;
+  }
+  if (error) {
+    return <h1>{error}</h1>;
+  }
   return (
     <ul>
       {todos.map((todo) => (
